3b979249b886018680b9f8fd624c466a104d01f0
authurton lunga <authurlunga@gmail.com>
2021-10-25 15:38:09 +0200

    Deploying to netlify

---
+++ src/data.js
@@ -0,0 +1,66 @@
+export const projects = [
+  {
+    title: "Vue & Django",
+    subtitle: "Django & Vue ecommorce",
+    description:
+      "Ecommerce app",
+    image: "./vue-store.png",
+    link: "https://github.com/Authurton",
+  },
+  {
+    title: "Vue Trader app",
+    subtitle: "Vue trading platform",
+    description:
+      "Trading platform app build using vue and vuex.",
+    image: "./trader1.png",
+    link: "http://trader-app.s3-website.us-east-2.amazonaws.com",
+  },
+  {
+    title: "DevChat",
+    subtitle: "React and Firebase",
+    description:
+      "Slack clone app, built using react.js, redux and firebase.",
+    image: "./project-3.gif",
+    link: "https://react-slack-clone-cb284.web.app/",
+  },
+  {
+    title: "Rental app",
+    subtitle: "Django",
+    description:
+      "Rental app built using django and bootstrap",
+    image: "./roomza1.png",
+    link: "https://Authurton1287.pythonanywhere.com",
+  },
+];
+
+export const testimonials = [
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/1.jpg",
+    name: "Jesse Hicks",
+    company: "Zoozle",
+  },
+  {
+    quote:
+      "Lorem ipsum dolor sit amet consectetur adipisicing elit. Culpa quod necessitatibus quidem impedit facilis? Distinctio laboriosam veritatis recusandae minus. Voluptatum accusantium hic laudantium soluta ea sapiente cupiditate officia maiores itaque.",
+    image: "https://randomuser.me/api/portraits/men/94.jpg",
+    name: "Ruben Alvarez",
+    company: "Dooble.io",
+  },
+];
+
+export const skills = [
+  "JavaScript",
+  "React",
+  "Redux",
+  "Vue.js",
+  "Axios",
+  "Vuex",
+  "Python",
+  "Django",
+  "Django RestFramework",
+  
+  
+
+];
